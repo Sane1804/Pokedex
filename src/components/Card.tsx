29cd7600d6055268cedddde81df9c387ef31e7a7
Alexander Vielma <vielmaa1804@gmail.com>
2024-03-21 09:54:31 -0400

    Remove useless code

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -14,54 +14,35 @@ interface CardInterface {
     types: string[]
 }
 
+
+
 interface PokemoDataInterface {
     pokemonData : CardInterface[],
     searched : string
 }
 
-type SubArray = CardInterface[]
-
-type SubArrElement = ReactElement[]
 
 export default function Card({pokemonData, searched}: PokemoDataInterface) {
     let copyPokemonData = pokemonData.slice().sort((a, b) => a.id - b.id)
-    let pokemonGroups: SubArray[] = []
-    let data: SubArrElement[] = []
-
-    for (let i = 0; i < copyPokemonData.length; i+=3){
-        pokemonGroups.push(copyPokemonData.slice(i, i+3))
-    }
-
-
+    let content;
     if (searched === ""){
-        for(let i = 0; i < pokemonGroups.length; i++){
-            data.push(pokemonGroups[i].map((item) => <div key={item.id} className="card">
+    content = copyPokemonData.map((item) => <div key={item.id} className="card">
+        <p>{`type ${item.types[0]}`}</p>
+        <figure>
+            <img src={item.sprites.front_default} alt="" />
+        </figure>
+        <h1>{item.name}</h1>
+    </div>)
+    } else {
+        let filteredData = copyPokemonData.filter((item) => item.name.includes(searched))
+        content = filteredData.map((item) => <div key={item.id} className="card">
+            <p>{`type ${item.types[0]}`}</p>
             <figure>
                 <img src={item.sprites.front_default} alt="" />
             </figure>
             <h1>{item.name}</h1>
-        </div>))
-        }
-    } else {
-        let copyData = pokemonGroups.filter((item) => {
-            for (let j = 0; j < item.length; j++){
-                if (item[j].name.includes(searched)){
-                    return item;
-                }
-            }
-        })
-        for (let i = 0; i < copyData.length; i++){
-            data.push(copyData[i].map((item) => <div key={item.id} className="card">
-                <figure>
-                    <img src={item.sprites.front_default} alt="" />
-                </figure>
-                <h1>{item.name}</h1>
-            </div>))
-        }
-
+        </div>)
     }
-
-    let content = data.map((item, index) => <div key={index} className="big-card">{item}</div>)
 
 
 
