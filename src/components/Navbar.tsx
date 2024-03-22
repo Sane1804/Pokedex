c3024c13565af0a6db8b6f363f91a1ea84729ebe
Alexander Vielma <vielmaa1804@gmail.com>
2024-03-21 23:20:57 -0400

    Made the logo clikeable

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,11 @@ import pokemonLogo from "../images/pokemon-logo.svg"
 export default function Navbar() {
   return (
     <nav id="navbar">
+      
       <div>
-        <img src={pokemonLogo} alt="pokemon logo" id="logo"/>
+        <Link to={"/"}><img src={pokemonLogo} alt="pokemon logo" id="logo"/></Link>
       </div>
+      
         <ul id="nav-list">
             <li className="nav-btn"><Link to={"/"}>Home</Link></li>
             <li className="nav-btn"><Link to={"/pokemons"}>Pokemons</Link></li>
